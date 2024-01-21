1ea4133993b620bb5ac84dd4d45b2f6f18bd9f98
rbompiani <bompiani@vt.edu>
2024-01-20 20:06:37 -0800

    added progress loader

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,17 @@
 import Page from "../components/Page";
+import NProgress from "nprogress";
+import Router from "next/router";
 
-export default function MyApp({Component, pageProps}) {
-    return <Page>
-        <Component {...pageProps} />
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+import "nprogress/nprogress.css";
+import "../components/styles/nprogress.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
     </Page>
+  );
 }
